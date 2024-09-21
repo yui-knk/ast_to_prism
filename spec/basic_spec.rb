52f28c7915cdb9bbf5f2eed71ccefc4952c3e239
ydah <t.yudai92@gmail.com>
2024-09-21 09:23:53 +0900

    Implement AliasGlobalVariable Node locations

--- spec/basic_spec.rb
+++ spec/basic_spec.rb
@@ -2082,7 +2082,9 @@ RSpec.describe "basic test cases return same nodes with prism" do
   end
 
   describe "valias" do
-    xit "tests" do
+    it "tests" do
+      pending "GlobalVariableReadNode locations are not supported"
+
       test_code("alias $y $x")
     end
   end
